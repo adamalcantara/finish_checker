a94446d9cfd0703b7691fea9b163e47306e72fe4
adamalcantara <alcantaradevelopment@gmail.com>
2021-06-25 14:34:05 -0400

    Alerting user if searchValue is blank

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
 let informationEl = document.getElementById("information");
 let searchBtn = document.getElementById("searchbtn")
+let blankSearch = document.getElementById("blanksearch")
 
 
 //Get the value put into the search box
 function searchValue() {
     let searchValue = document.querySelector("#search-value").value;
     console.log(searchValue);
+
+    //Check if searchValue is blank, and tell user to input a search term
+    if (searchValue == "") {
+        let pleaseInput = "Please input a search term."
+        console.log(pleaseInput);
+        blankSearch.append(pleaseInput);
+    }
 }
 
 //Call the searchValue function when the search button is clicked
